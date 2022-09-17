363143ae16596f5b285e52e2d93362945af29ab1
benop7 <benjaminpetegnong@gmail.com>
2022-09-17 03:59:37 +0100

    Revert "task 6"
    This reverts commit b1f749bc6559165647d016b357d7d6d49d9fc6f7.

--- models/city.py
+++ models/city.py
@@ -1,22 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Relationship, Base
-from sqlalchemy import Column, String, ForeignKey
-from os import getenv
+from models.base_model import BaseModel
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    if storage_type == 'db':
-        __tablename__ = 'cities'
-
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = Relationship('Place', cascade='all, delete', backref='cities')
-    else:
-        state_id = ""
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes"""
-        super().__init__(*args, **kwargs)
+    state_id = ""
+    name = ""

e178ac2f264be6512ae36ec9bf0c8ed723b8ca62
benop7 <benjaminpetegnong@gmail.com>
2022-09-17 03:33:25 +0100

    task 4

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,16 +4,18 @@
 --   Grants all privileges for hbnb_dev on hbnb_dev_db.
 --   Grants SELECT privilege for hbnb_dev on performance_schema.
 
+-- Create hbnb_dev_db database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_dev_db`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+-- Switch to hbnb_dev_db database
+USE hbnb_dev_db;
+
+-- Create user hbnb_dev
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Grant usage on hbnb_dev_db to hbnb_dev
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+-- Grant Select previledges on performance_schema to hbnb_dev
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+-- Grant all previledges on hbnb_dev_db to hbnb_dev
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+-- Flush all previledges
 FLUSH PRIVILEGES;

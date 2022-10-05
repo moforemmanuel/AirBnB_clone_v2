85723f379f4b78ab2c1212ab577906fdb7024c56
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-05 18:29:54 +0100

    improved city model

--- models/city.py
+++ models/city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.state import State
+# from models.state import State
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv

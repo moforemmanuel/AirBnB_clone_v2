3ae39b42d1d803104a9c883d654e1fca6f8ce38a
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-05 13:39:20 +0100

    change state to adapt to DBstorage

--- models/state.py
+++ models/state.py
@@ -1,8 +1,35 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    # name = ""
+    __tablename__ = 'states'
+    if storage_type == 'db':
+        name = Column(String(60), nullable=False)
+        # children = relationship("City")
+        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    else:
+        name = ""
+
+    if storage_type != 'db':
+        @property
+        def cities(self):
+            """ get list of city instances with state_id
+                equals current state.id
+             """
+            list_cities = []
+            all_cities = models.storage.all(City);
+            for key, city_obj in all_cities.items():
+                if city_obj.state_id == self.id:
+                    list_cities.append(city_obj)
+            return list_cities

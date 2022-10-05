09a93b11885a45fa881c44d4097dbf1ac7634266
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-05 12:06:55 +0100

    change base_model to adapt to DBstorage

--- models/base_model.py
+++ models/base_model.py
@@ -2,12 +2,24 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+
+import models
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    # __tablename__ = 'BaseModel'
+
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
         id = str(uuid.uuid4())
         created_at = datetime.now()
         updated_at = datetime.now()
@@ -17,7 +29,12 @@ class BaseModel:
             self.id = id
             self.created_at = created_at
             self.updated_at = updated_at
-            storage.new(self)
+            # storage.new(self)
+
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
         else:
             try:
                 kwargs['updated_at'] = datetime.fromisoformat(
@@ -40,7 +57,8 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -50,4 +68,12 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary.keys():
+            # use del instead of pop, don't leak mem
+            del dictionary['_sa_instance_state']
         return dictionary
+
+    def delete(self):
+        """ Delete current instance from storage """
+        models.storage.delete(self)
+

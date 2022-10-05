b40a3a9df2022087df1852a5f0f768a6ff285462
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-05 18:29:18 +0100

    improved console.py

--- console.py
+++ console.py
@@ -142,9 +142,10 @@ class HBNBCommand(cmd.Cmd):
                 if value is not None:
                     setattr(new_instance, key, value)
 
-        storage.save()
+        # storage.save()
         print(new_instance.id)
-        storage.save()
+        new_instance.save()
+        # storage.save()
 
     def string_parser(self, param):
         """Handles the params and check if its valid"""
@@ -255,14 +256,15 @@ class HBNBCommand(cmd.Cmd):
 
         if args:
             args = args.split(' ')[0]  # remove possible trailing args
+            # print(args)
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all(args).items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 print_list.append(str(v))
 
         print(print_list)
@@ -275,7 +277,7 @@ class HBNBCommand(cmd.Cmd):
     def do_count(self, args):
         """Count current number of class instances"""
         count = 0
-        for k, v in storage._FileStorage__objects.items():
+        for k, v in storage.all().items():
             if args == k.split('.')[0]:
                 count += 1
         print(count)

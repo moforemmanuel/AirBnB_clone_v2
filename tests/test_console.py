07570c83e9269b22b7dab038f4bf1b74c29fd287
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-06 22:58:13 +0100

    show

--- tests/test_console.py
+++ tests/test_console.py
@@ -6,7 +6,8 @@ import sys
 import models
 import unittest
 from io import StringIO
-from unittest.mock import create_autospec
+from unittest.mock import create_autospec, patch
+from uuid import UUID
 
 
 class TestConsole(unittest.TestCase):
@@ -31,7 +32,106 @@ class TestConsole(unittest.TestCase):
         """
         return HBNBCommand()
 
-    def test_quit
+    # def test_quit(self):
+    #     """ Test quit exists """
+    #     console = self.create()
+    #     # console.onecmd("quit")
+    #     # self.assertTrue(console.onecmd("quit"))
+    #     console.onecmd("quit")
+    #     self.assertTrue(self.capt_out.getvalue(), None)
+
+    # def test_EOF(self):
+    #     """ Test EOF exists """
+    #     console = self.create()
+    #     self.assertTrue(console.onecmd("EOF"))
+    #     self.assertTrue(isinstance(self.capt_out.getvalue(), str))
+
+    def test_all(self):
+        """ test for all """
+        console = self.create()
+        console.onecmd("all")
+        self.assertTrue(isinstance(self.capt_out.getvalue(), str))
+
+    @unittest.skipIf(
+        os.getenv('HBNB_TYPE_STORAGE') == 'db',
+        "This wont work for db storage"
+    )
+    def test_show(self):
+        """
+        test for show
+        """
+        console = self.create()
+        console.onecmd("create User")
+        user_id = self.capt_out.getvalue()
+        # break output and do onecmd
+        sys.stdout = self.backup
+        self.capt_out.close()
+        self.capt_out = StringIO()
+        sys.stdout = self.capt_out
+        # continue
+        console.onecmd("show User " + user_id)
+        out = self.capt_out.getvalue()
+        self.assertTrue(isinstance(out, str))
+
+    @unittest.skipIf(
+        os.getenv('HBNB_TYPE_STORAGE') == 'db',
+        "This wont work for db storage"
+    )
+    def test_show_missing_classname(self):
+        """
+        test for show missing class name
+        """
+        console = self.create()
+        console.onecmd("create User")
+        user_id = self.capt_out.getvalue()
+        # break output and do onecmd
+        sys.stdout = self.backup
+        self.capt_out.close()
+        self.capt_out = StringIO()
+        sys.stdout = self.capt_out
+        # continue
+        console.onecmd("show")
+        out = self.capt_out.getvalue()
+        self.assertEqual("** class name missing **\n", out)
+
+    def test_show_missing_id(self):
+        """
+        test for show missing instance id
+        """
+        console = self.create()
+        console.onecmd("create User")
+        user_id = self.capt_out.getvalue()
+        # break output and do onecmd
+        sys.stdout = self.backup
+        self.capt_out.close()
+        self.capt_out = StringIO()
+        sys.stdout = self.capt_out
+        # continue
+        console.onecmd("show User")
+        out = self.capt_out.getvalue()
+        self.assertEqual("** instance id missing **\n", out)
+
+    def test_show_no_instance(self):
+        """
+        test for show no instance
+        """
+        console = self.create()
+        console.onecmd("create User")
+        user_id = self.capt_out.getvalue()
+        # break output and do onecmd
+        sys.stdout = self.backup
+        self.capt_out.close()
+        self.capt_out = StringIO()
+        sys.stdout = self.capt_out
+        # continue
+        console.onecmd("show User 1234")
+        out = self.capt_out.getvalue()
+        self.assertEqual("** no instance found **\n", out)
+
+
+
+# with patch('sys.stdout', new=StringIO()) as f:
+#     HBNBCommand.onecmd("help show")
 
 if __name__ == '__main__':
     unittest.main()

3cd8cdf171b1f8848a053672b474fdb679c87af7
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-06 18:07:53 +0100

    removed PK contraints from place_amenities

--- models/place.py
+++ models/place.py
@@ -16,11 +16,13 @@ if storage_type == 'db':
     place_amenity = Table(
         'place_amenity',
         metadata,
-        Column('place_id', String(60),
+        Column('place_id',
+               String(60),
                ForeignKey('places.id'),
                # primary_key=True,
                nullable=False),
-        Column('amenity_id', String(60),
+        Column('amenity_id',
+               String(60),
                ForeignKey('amenities.id'),
                # primary_key=True,
                nullable=False)

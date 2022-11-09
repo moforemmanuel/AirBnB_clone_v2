4573488e3bfea48cab0edb08e6576ce752ccb919
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-11-09 15:43:07 +0100

    deploy web static fix 13

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -38,3 +38,33 @@ def do_deploy(archive_path):
     """
     if not os.path.exists(archive_path):
         return False
+    try:
+        # archive_path=versions/web_static_20170315003959.tgz
+        tarball = archive_path.split("/")[-1]
+        tar_no_ext = tarball.split(".")[0]
+        # tar_folder = archive_path.split("/")[-2]
+
+        # upload file to /tmp/
+        put(archive_path, "/tmp/")
+
+        # decompress
+        run(f"mkdir -p /data/web_static/releases/{tar_no_ext}/")
+        run(f"tar -xzf /tmp/{tarball} -C /data/web_static/releases/{tar_no_ext}")
+
+        # delete archive
+        run(f"rm /tmp/{tarball}")
+
+        # delete symlink /data/web_static/current
+        run(f"mv /data/web_static/releases/{tar_no_ext}/web_static/* /data/web_static/releases/{tar_no_ext}/")
+        run(f"rm -rf /data/web_static/releases/{tar_no_ext}/web_static")
+        run("rm -rf /data/web_static/current")
+
+        # recreate symlink /data/web_static/current
+        # link to /data/web_static/releases/<archive filename without extension>
+        run(
+            "ln -s /data/web_static/releases/{tar_no_ext}/ /data/web_static/current")
+        run("echo 'New version deployed!'")
+        return True
+
+    except:
+        return False

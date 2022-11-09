7b4e2697cf775ef38485e60029ae0755424c24bd
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-11-09 14:04:45 +0100

    pack web_static to versioned tarball

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""pack files"""
+
+from datetime import datetime
+from fabric.api import run, local
+
+
+def do_pack():
+    """
+    Gen tarball from web_static
+    Return path if successfull, else None
+    """
+    suffix = datetime.now().strftime("%Y%m%d%H%M%S")
+    filePath = f"versions/web_static_{suffix}.tgz"
+
+    # create versions folder
+    local("mkdir -p versions/")
+
+    # pack web_static
+    tarball = local(f"tar -cvzf {filePath} web_static/")
+
+    if tarball.succeeded:
+        return filePath
